751254bc71ce7147df95a9646f5921720231bc64
FireWindCyclone <esh716@gmail.com>
2026-07-28 03:29:21 +0530

    feat(api): setup cors

--- backend/src/main/java/game/ttt/config/WebConfig.java
+++ backend/src/main/java/game/ttt/config/WebConfig.java
@@ -1,14 +1,29 @@
 package game.ttt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", _ -> true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET", "PUT", "POST")
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
 
 }
